2ea8c43a5c010826ba86e39383652ca1815fbf48
Jorge Mercado Ferreño <jorgemercadof@hotmail.com>
2019-02-07 02:22:05 +0100

    master

---
+++ WebSiteRestaurante/Scripts/Pedido.js
@@ -0,0 +1,15 @@
+﻿function handleDrag(event) {
+    event.dataTransfer.effectAllowed = "copy";
+    event.dataTransfer.setData("text/html", event.target.innerHTML);
+}
+function handleDragOver(event) {
+    event.stopPropagation();
+    event.preventDefault();
+    event.dataTransfer.dropEffect = "copy"; // Display a "copy" cursor
+}
+function handleDrop(dropEvent) {
+    dropEvent.stopPropagation();
+    dropEvent.preventDefault();
+    var data = dropEvent.dataTransfer.items;
+    dropEvent.target.appendChild(data.innerHTML);
+}
